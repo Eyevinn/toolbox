6f09293a9a4998d1eaa2117e927859c83b4e4655
birme <jonas.birme@eyevinn.se>
2020-01-26 14:32:38 +0100

    Support for adding a channel bug/logo

--- python/hls2ts.py
+++ python/hls2ts.py
@@ -17,6 +17,7 @@ parser = argparse.ArgumentParser(description='Pull live HLS and output to multic
 parser.add_argument('hlsurl')
 parser.add_argument('outputaddress')
 parser.add_argument('--srt', action='store_true', help='use SRT as transport protocol')
+parser.add_argument('--channelbug', help='overlay logo as channel bug (poc)')
 parser.add_argument('--bitrate', help='which bitrate to use')
 parser.add_argument('--with-debug', dest='debug', action='store_true')
 args = parser.parse_args()
@@ -30,10 +31,14 @@ if args.bitrate:
   bitrate = "-map m:variant_bitrate:%s" % args.bitrate
 
 ffmpeg = "ffmpeg -fflags +genpts -re -i %s %s -strict -2 -y -vcodec copy -f mpegts %s://%s?pkt_size=1316" % (args.hlsurl, bitrate, protocol, args.outputaddress)
+if args.channelbug:
+  ffmpeg = "ffmpeg -fflags +genpts -re -i %s -r 25 -i %s -filter_complex [0:1][1:v]#overlay=10:10:eval=init -c:v libx264 -preset ultrafast -strict -2 -y -f mpegts -max_muxing_queue_size 2048 %s://%s?pkt_size=1316" % (args.hlsurl, args.channelbug, protocol, args.outputaddress)
 
+
+arglist = ffmpeg.split()
 if args.debug:
   print "%s" % ffmpeg
-  print ffmpeg.split()
+  print map(lambda x: x.replace("#", " "), arglist)
 
-p1 = subprocess.Popen(ffmpeg.split())
+p1 = subprocess.Popen(map(lambda x: x.replace("#", " "), arglist))
 output,err = p1.communicate()
